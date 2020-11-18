d608023424f6b39c0b1ef25e921e088a0e8bfaee
AyumiOsawa <a.osawa1002@gmail.com>
2020-11-18 16:25:29 +0200

    part3, exercise 3.2

--- index.js
+++ index.js
@@ -21,12 +21,55 @@ const persons = [
   {
     "name": "Mary Poppendieck",
     "number": "39-23-6423122",
-    "id": 4
+    "id": 41
   }
 ]
 
 app.get('/api/persons', (request, response) => {
   response.json(persons);
+})
+
+app.get('/info', (request, response) => {
+  const num = persons.length;
+  const num_text = `<div>Phonebook has info for ${num} people<div><br>`;
+  const timestamp = new Date();
+  const days = [
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat"
+  ];
+  const day = days[ timestamp.getDay() ];
+  const date = timestamp.getDate();
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
+  const month = months[ timestamp.getMonth() ];
+  const fullYear = timestamp.getFullYear();
+  const h = timestamp.getHours();
+  const m = timestamp.getMinutes();
+  const s = timestamp.getSeconds();
+  const timeDiff = timestamp.getTimezoneOffset()*-1/60;
+  const date_text = `<div>${day} ${month} ${date} ${fullYear}
+    ${h}:${m}:${s} GMT${timeDiff > 0 ? '+' : ''}${timeDiff} (Eastern European 
+    Standard Time)</div>`;
+  const text = num_text + date_text;
+
+  response.send(text);
 })
 
 const PORT = 3001;
